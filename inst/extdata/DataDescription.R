1547736cb7612699f4d73dee95e260d15ce2e4b3
Jake <github@jakewendt.com>
2019-12-03 07:50:16 -0800

    Added sample data

---
+++ inst/extdata/DataDescription.R
@@ -0,0 +1,16 @@
+#' Raw Example Dataset
+#' 
+#' The dataset contains pseudo mzXML files of three species (A,B,C) with two different strains per species.
+#' The dataset also contains description files of given mzXML files.
+#' There are 23 mzXML files (10 files for species A, 7 files for species B, and 6 files for species C).
+#' 
+#' The list files contains data descriptions. For example, listA.txt contains a file description for species A. 
+#' There are three list files for species A, B, and C: listA.txt, listB.txt, listC.txt.
+#' 
+#' @format The list files (e.g. listA.txt) contains three columns:
+#' \descrbie{
+#' \item{file.name}{Names of mzXML files.}
+#' \item{strain.no}{Positive integers starting from one.}
+#' \item{strain}{Strain names.}
+#' }
+#' 
